f64b21c2c449146ac113ac04b468be7a7477232e
Peter Lollo <peteralollo@gmail.com>
2016-02-19 19:56:26 -0800

    Implement unit testing

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -6,9 +6,14 @@ describe('the todo.App', function() {
   describe('the todo object', function(){
 
     it('should have all the necessary methods', function(){
+      expect(todo).to.have.property('setup');
+      expect(todo).to.have.property('init');
     });
   });
 });
 
 describe('the todo.util methods', function() {
+  assert(todo.util.trimTodoName('hi ') === 'hi', 'white space gets trimmed out of todos');
+  todo.util.isValidTodoName('a').should.equal(false);
+  assert.typeOf(todo.util.getUniqueId(), 'number', 'getUniqueId returns a number');
 });
